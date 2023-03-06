4a695eb056a7e69401bdd70ee3d930b0807b42ce
Anna Kirillova <a.kirillova1812@gmail.com>
2023-03-06 19:38:52 +0400

    Parsing from file was added

--- src/Day24/Part1.java
+++ src/Day24/Part1.java
@@ -1,54 +1,25 @@
 package Day24;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class Part1 {
-    public static void main(String[] args) {
+
+    final static boolean TEST_MODE = false;
+
+    public static void main(String[] args) throws IOException {
 
         List<Group> immuneSystem = new ArrayList<>();
-
-        Set<AttackType> weaknesses = new HashSet<>();
-        weaknesses.add(AttackType.RADIATION);
-        weaknesses.add(AttackType.BLUDGEONING);
-
-        Group group1 = new Group(5390, 4507, AttackType.FIRE, 2, new HashSet<>(), weaknesses,17);
-
-        weaknesses = new HashSet<>();
-        weaknesses.add(AttackType.BLUDGEONING);
-        weaknesses.add(AttackType.SLASHING);
-
-        Set<AttackType> immunities = new HashSet<>();
-        immunities.add(AttackType.FIRE);
-
-        Group group2 = new Group(1274, 25, AttackType.SLASHING, 3, immunities, weaknesses, 989);
-
-        immuneSystem.add(group1);
-        immuneSystem.add(group2);
-
         List<Group> infection = new ArrayList<>();
 
-        weaknesses = new HashSet<>();
-        weaknesses.add(AttackType.RADIATION);
-
-        Group group3 = new Group(4706, 116, AttackType.BLUDGEONING, 1, new HashSet<>(), weaknesses, 801);
-
-        immunities = new HashSet<>();
-        immunities.add(AttackType.RADIATION);
-
-        weaknesses = new HashSet<>();
-        weaknesses.add(AttackType.FIRE);
-        weaknesses.add(AttackType.COLD);
-
-        Group group4 = new Group(2961, 12, AttackType.SLASHING, 4, immunities, weaknesses, 4485);
-
-        infection.add(group3);
-        infection.add(group4);
-
         Map<Integer, Group> groupWithId = new HashMap<>();
-        groupWithId.put(group1.getGroupNumber(), group1);
-        groupWithId.put(group2.getGroupNumber(), group2);
-        groupWithId.put(group3.getGroupNumber(), group3);
-        groupWithId.put(group4.getGroupNumber(), group4);
+        if (TEST_MODE) {
+            fillByTestData(immuneSystem, infection, groupWithId);
+        } else {
+            fillByRealData(immuneSystem, infection, groupWithId);
+        }
 
         TreeSet<Group> immuneTargetSelectionQueue = new TreeSet<>(new TargetSelectionComparator());
         immuneTargetSelectionQueue.addAll(immuneSystem);
@@ -88,7 +59,115 @@ public class Part1 {
         }
 
         System.out.println(counter);
-        
+
+    }
+
+    private static void fillByRealData(List<Group> immuneSystem, List<Group> infection, Map<Integer, Group> groupWithId) throws IOException {
+
+        String path = ".\\src\\Day24\\data.txt";
+        final String[] instructions = Files.readString(Path.of(path)).split("\r\n\r\n");
+
+        for (String system : instructions) {
+            String[] systemGroup = system.split("\r\n");
+            for (int i = 0; i < systemGroup.length; i++) {
+                if (i == 0) {
+                    continue;
+                }
+
+                final String regex1 = "(\\d+) units each with (\\d+) hit points ";
+                int units = Integer.parseInt(systemGroup[i].replaceAll(regex1, "$1"));
+                int hitPoints = Integer.parseInt(systemGroup[i].replaceAll(regex1, "$2"));
+
+                final String regex2 = " with an attack that does (\\d+) (\\w+) damage at initiative (\\d+)";
+                int attackDamage = Integer.parseInt(systemGroup[i].replaceAll(regex2, "$1"));
+                AttackType attackType = AttackType.valueOf(systemGroup[i].replaceAll(regex2, "$2").toUpperCase());
+                int initiative = Integer.parseInt(systemGroup[i].replaceAll(regex2, "$3"));
+
+                final String regex3 = "immune to ([a-z, ]+)";
+                String[] immuneFeature = systemGroup[i].replaceAll(regex3, "$1").split(", ");
+                Set<AttackType> immunities = new HashSet<>();
+                for (String feature : immuneFeature) {
+                    immunities.add(AttackType.valueOf(feature.toUpperCase()));
+                }
+
+                final String regex4 = "weak to ([a-z, ]+)";
+                String[] weakFeature = systemGroup[i].replaceAll(regex4, "$1").split(", ");
+                Set<AttackType> weaknesses = new HashSet<>();
+                for (String feature : weakFeature) {
+                    weaknesses.add(AttackType.valueOf(feature.toUpperCase()));
+                }
+
+                Group group = new Group(hitPoints, attackDamage, attackType, initiative, immunities, weaknesses, units);
+
+                if (systemGroup[0].equals("Immune System:")) {
+                    immuneSystem.add(group);
+                } else {
+                    infection.add(group);
+                }
+
+                groupWithId.put(group.getGroupNumber(), group);
+            }
+        }
+
+
+//        final String way = Files.readString(Path.of(path));
+
+//        String path = ".\\src\\Day6\\data.txt";
+//        final String[] instructions = Files.readString(Path.of(path)).lines().toArray(String[]::new);
+//
+//        for (String str : instructions) {
+//            final String regex = "^(toggle|turn off|turn on) (\\d+,\\d+) (through) (\\d+,\\d+)$";
+//
+//            String todo = str.replaceAll(regex, "$1");
+//            String[] coord1 = str.replaceAll(regex, "$2").split(",");
+//            String[] coord2 = str.replaceAll(regex, "$4").split(",");
+//
+//            setUpLights(todo, coord1, coord2);
+//        }
+
+    }
+
+    private static void fillByTestData(List<Group> immuneSystem, List<Group> infection, Map<Integer, Group> groupWithId) {
+
+        Set<AttackType> weaknesses = new HashSet<>();
+        weaknesses.add(AttackType.RADIATION);
+        weaknesses.add(AttackType.BLUDGEONING);
+
+        Group group1 = new Group(5390, 4507, AttackType.FIRE, 2, new HashSet<>(), weaknesses,17);
+
+        weaknesses = new HashSet<>();
+        weaknesses.add(AttackType.BLUDGEONING);
+        weaknesses.add(AttackType.SLASHING);
+
+        Set<AttackType> immunities = new HashSet<>();
+        immunities.add(AttackType.FIRE);
+
+        Group group2 = new Group(1274, 25, AttackType.SLASHING, 3, immunities, weaknesses, 989);
+
+        immuneSystem.add(group1);
+        immuneSystem.add(group2);
+
+        weaknesses = new HashSet<>();
+        weaknesses.add(AttackType.RADIATION);
+
+        Group group3 = new Group(4706, 116, AttackType.BLUDGEONING, 1, new HashSet<>(), weaknesses, 801);
+
+        immunities = new HashSet<>();
+        immunities.add(AttackType.RADIATION);
+
+        weaknesses = new HashSet<>();
+        weaknesses.add(AttackType.FIRE);
+        weaknesses.add(AttackType.COLD);
+
+        Group group4 = new Group(2961, 12, AttackType.SLASHING, 4, immunities, weaknesses, 4485);
+
+        infection.add(group3);
+        infection.add(group4);
+
+        groupWithId.put(group1.getGroupNumber(), group1);
+        groupWithId.put(group2.getGroupNumber(), group2);
+        groupWithId.put(group3.getGroupNumber(), group3);
+        groupWithId.put(group4.getGroupNumber(), group4);
     }
 
     private static void targetSelection(TreeSet<Group> whoAttack, List<Group> whoDefends){
