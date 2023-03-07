d5ef66546d03fb60a338188727263e60d47e4447
Anna Kirillova <a.kirillova1812@gmail.com>
2023-03-07 19:12:23 +0400

    rename main class

--- src/Day24/Solution.java
+++ src/Day24/Solution.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Part1 {
+public class Solution {
 
     final static boolean TEST_MODE = false;
 
